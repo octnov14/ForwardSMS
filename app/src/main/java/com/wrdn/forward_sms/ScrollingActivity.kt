2868cc56d1bc781f2cf9b32afbcc9db23606dc4f
J.M.Yoon <octnov14@gmail.com>
2020-09-06 16:43:07 +0900

    중간 저장

--- app/src/main/java/com/wrdn/forward_sms/ScrollingActivity.kt
+++ app/src/main/java/com/wrdn/forward_sms/ScrollingActivity.kt
@@ -3,7 +3,6 @@ package com.wrdn.forward_sms
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Telephony
@@ -14,6 +13,10 @@ import androidx.core.content.ContextCompat
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -45,26 +48,133 @@ class ScrollingActivity : AppCompatActivity() {
 
 
 
-        haha(this)
+        readMMS(this)
+        readSMS(this)
 
     }
 
-    private fun haha(context: Context) {
-        val cursor = context.contentResolver.query(Telephony.Sms.CONTENT_URI, null, null, null, null)
+    // 참조 URL
+    // https://www.it-swarm.dev/ko/android/mms-android%EC%9D%98-%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%A5%BC-%EC%9D%BD%EB%8A%94-%EB%B0%A9%EB%B2%95/969694767/
+
+    private fun readMMS(context: Context) {
+        val uri = Telephony.Mms.Inbox.CONTENT_URI
+
+        val proj = arrayOf("*")
+        val cursor = context.contentResolver.query(uri, proj, null, null, "_id desc")
+
+        var n = 0
+        if (cursor != null) {
+            cursor.moveToFirst()
+
+            do {
+                // mms columns
+                // date 1970년으로 다 똑같다 -> 무의미
+                // 아이디, 수신번호만 의미있다
+                // ct_t 는 컨텐트 타입이다
+                // Available columns: [_id, thread_id, date, date_sent, msg_box, read, m_id, sub, sub_cs, ct_t, ct_l, exp,
+                // m_cls, m_type, v, m_size, pri, rr, rpt_a, resp_st, st, tr_id, retr_st, retr_txt, retr_txt_cs,
+                // read_status, ct_cls, resp_txt, d_tm, d_rpt, locked, seen, sub_id, phone_id, creator,
+                // imsi_data, group_id, save_call_type, msg_boxtype, type, address, name, tag, tag_eng,
+                // spam_report, reserve_time, insert_time, sender_num, textlink, text_only, c0_iei,
+                // kt_tm_send_type, line_address, notification]
+
+                val id = cursor.getString(cursor.getColumnIndexOrThrow("_id"))
+                val number = cursor.getString(cursor.getColumnIndexOrThrow("address"))
+                val body = getMMSBody(id)
+
+
+                System.out.printf("%s; %s\n", number, body)
+
+                if(n++ > 300) break
+
+            } while (cursor.moveToNext())
+
+            cursor.close()
+        }
+    }
+
+    private fun getMMSBody(mmsId: String): String {
+        val selectionPart = "mid=$mmsId"
+        val uri = Uri.parse("content://mms/part")
+        val cursor = contentResolver.query(uri, null, selectionPart, null, null)
+
+        var body = ""
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+
+
+                    val partId = cursor.getString(cursor.getColumnIndex("_id"))
+                    val type = cursor.getString(cursor.getColumnIndex("ct"))
+
+                    if ("text/plain" == type) {
+                        val data: String? = cursor.getString(cursor.getColumnIndex("_data"))
+                        if (data != null) {
+                            // implementation of this method below
+                            body = getMmsText(partId)
+                        } else {
+                            body = cursor.getString(cursor.getColumnIndex("text"))
+                        }
+                    }
+                } while (cursor.moveToNext())
+
+                cursor.close()
+            }
+        }
+
+        return body.replace("""\s""".toRegex(), " ")
+    }
+
+    private fun getMmsText(id: String): String {
+        val partURI = Uri.parse("content://mms/part/$id")
+        var `is`: InputStream? = null
+        val sb = StringBuilder()
+        try {
+            `is` = contentResolver.openInputStream(partURI)
+            if (`is` != null) {
+                val isr = InputStreamReader(`is`, "UTF-8")
+                val reader = BufferedReader(isr)
+                var temp: String = reader.readLine()
+                while (temp != null) {
+                    sb.append(temp)
+                    temp = reader.readLine()
+                }
+            }
+        } catch (e: IOException) {
+        } finally {
+            if (`is` != null) {
+                try {
+                    `is`.close()
+                } catch (e: IOException) {
+                }
+            }
+        }
+        return sb.toString()
+    }
+
+
+    private fun readSMS(context: Context) {
+        val uri = Telephony.Sms.Inbox.CONTENT_URI  // Uri.parse("content://sms/inbox")
+
+        val cursor = context.contentResolver.query(uri, null, null, null, null)
 
         if (cursor != null && cursor.moveToFirst()) {
             do {
-                val smsDate = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.DATE))
-                val number = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.ADDRESS))
-                val body = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.BODY))
+                val smsDate = cursor.getString(cursor.getColumnIndexOrThrow("date"))
+                val number = cursor.getString(cursor.getColumnIndexOrThrow("address"))
+                val body = cursor.getString(cursor.getColumnIndexOrThrow("body"))
 
-                System.out.printf("%s; %s; %s\n", Date(smsDate.toLong()), number, body.replace("\\s".toRegex()," "))
+                val yc = YCalendar(smsDate)
+
+
+                System.out.printf("%s; %s; %s; %s\n", Date(smsDate.toLong()), yc, number, body.replace("""\s""".toRegex(), " "))
 
             } while (cursor.moveToNext())
         }
 
         cursor?.close()
     }
+
 
     private fun sample__getSmsConversation() {
         getSmsConversation(this, "15447200") { conversations ->
@@ -157,6 +267,8 @@ class ScrollingActivity : AppCompatActivity() {
             }
         }
     }
+
+
 /*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -175,4 +287,6 @@ class ScrollingActivity : AppCompatActivity() {
         }
     }
 */
+
+
 }
---
+++ app/src/main/java/com/wrdn/forward_sms/YCalendar.kt
@@ -0,0 +1,466 @@
+package com.wrdn.forward_sms
+
+import android.content.Intent
+import java.text.ParsePosition
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class YCalendar : GregorianCalendar {
+
+    private var agd_name: String? = null
+    private var agd_action1_name: String? = null
+    private var agd_action_name: String? = null
+    private var agd_action_time: String? = null
+
+    fun setAGDInfo(
+        groupName: String?,
+        action1Name: String?,
+        actionName: String?,
+        actionTime: String?
+    ) {
+        agd_name = groupName
+        agd_action1_name = action1Name
+        agd_action_name = actionName
+        agd_action_time = actionTime
+    }
+
+    init {
+        firstDayOfWeek = MONDAY
+        minimalDaysInFirstWeek = 7
+    }
+
+    constructor() : super() {}
+
+    constructor(cal: Calendar) {
+        setTime(cal.time)
+    }
+
+    constructor(d: Date) {
+        setTime(d)
+    }
+
+    constructor(format: String, s: String) {
+        try {
+            SimpleDateFormat(format, Locale.getDefault()).parse(s, ParsePosition(0))?.let {
+                setTime(it)
+            }
+        } catch (e: Exception) {
+        }
+    }
+
+    constructor(s: String) {
+        try {
+            when (s.length) {
+                8 -> SimpleDateFormat("yyyyMMdd", Locale.getDefault()).parse(s, ParsePosition(0))?.let {
+                    setTime(it)
+                }
+
+                14 -> SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).parse(
+                    s,
+                    ParsePosition(0)
+                )?.let {
+                    setTime(it)
+                }
+            }
+        } catch (e: Exception) {
+        }
+    }
+
+    constructor(year: Int, month: Int) : this(year, month, 1) {}
+
+    constructor(year: Int, month: Int, date: Int) : super(year, month, date) {}
+
+    constructor(year: Int, month: Int, date: Int, hour: Int, minute: Int) : super(
+        year, month, date, hour, minute
+    ) {
+    }
+
+    constructor(
+        year: Int,
+        month: Int,
+        date: Int,
+        hour: Int,
+        minute: Int,
+        second: Int
+    ) : super(year, month, date, hour, minute, second) {
+
+    }
+
+
+    fun equals(cal: YCalendar): Boolean {
+        return compareTo(cal) == 0
+    }
+
+    fun equalsDate(cal: YCalendar): Boolean {
+        return getYYYYMMDD() == cal.getYYYYMMDD()
+    }
+
+    override fun compareTo(other: Calendar): Int {
+        return getTime().compareTo(other.time)
+    }
+
+
+    private fun getYear(): Int {
+        return get(YEAR)
+    }
+
+    private fun getMonth(): Int {
+        return get(MONTH)
+    }
+
+    private fun getDate(): Int {
+        return get(DATE)
+    }
+
+    fun getHour(): Int {
+        return get(HOUR)
+    }
+
+    private fun getHour24(): Int {
+        return get(HOUR_OF_DAY)
+    }
+
+    private fun getMinute(): Int {
+        return get(MINUTE)
+    }
+
+    fun getSecond(): Int {
+        return get(SECOND)
+    }
+
+    fun getDayOfWeek(): Int {
+        return get(DAY_OF_WEEK)
+    }
+
+    private fun setYear(t: Int) {
+        set(YEAR, t)
+    }
+
+    private fun setMonth(t: Int) {
+        set(MONTH, t)
+    }
+
+    fun setDate(t: Int) {
+        set(DATE, t)
+    }
+
+    private fun setHour(t: Int) {
+        set(HOUR_OF_DAY, t)
+    }
+
+    private fun setMinute(t: Int) {
+        set(MINUTE, t)
+    }
+
+    fun setSecond(t: Int) {
+        set(SECOND, t)
+    }
+
+    private fun setYMD(y: Int, m: Int, d: Int) {
+        setYear(y)
+        setMonth(m)
+        setDate(d)
+    }
+
+    fun setHMS(h: Int, m: Int, s: Int) {
+        setHour(h)
+        setMinute(m)
+        setSecond(s)
+    }
+
+    private fun format(s: String): String {
+        return try {
+            SimpleDateFormat(s, Locale.getDefault()).format(getTime())
+        } catch (e: Exception) {
+            ""
+        }
+    }
+
+    fun getYYYYMM(): String {
+        return format("yyyyMM")
+    }
+
+    fun getYYYYMMDD(): String {
+        return format("yyyyMMdd")
+    }
+
+    fun getYYYY_MM_DD(): String {
+        return format("yyyy-MM-dd")
+    }
+
+    fun getYMDE(): String {
+        return format("yyyy-MM-dd(EEE)")
+    }
+
+    fun getYMDHMS(): String {
+        return format("yyyyMMddHHmmss")
+    }
+
+    fun getYMDHMSSql(): String {
+        return format("yyyy-MM-dd HH:mm:ss")
+    }
+
+
+    fun getTimeGapStr(): String {
+        val now = YCalendar()
+
+        var h = getHour24() - now.getHour24()
+        var m = getMinute() - now.getMinute()
+
+        if (m < 0) {
+            m = 60 + m
+            h--
+        }
+
+        return if (h > 0) "${h}시간 ${m}분 전"
+        else "${m}분 전"
+    }
+
+    fun getDayStr(): String {
+        val today = YCalendar()
+
+        val dStr = when (val n = getDaysBetween(today, this)) {
+            0 -> "오늘"
+            1 -> "내일"
+            2 -> "모레"
+            else -> "" + n + "일 후"
+        }
+
+        return dStr
+
+    }
+
+    fun getMinuteStr(): String {
+        val mStr = when (val m = getMinute()) {
+            0 -> "정각"
+            else -> "" + m + "분"
+        }
+
+        return mStr
+    }
+
+    override fun toString(): String {
+        return format("yyyy-MM-dd(E) HH:mm:ss")
+    }
+
+
+    //
+    // 날짜연산
+    //
+
+    //
+// 날짜연산
+//
+    fun addDate(d: Int): YCalendar {
+        add(DATE, d)
+        return this
+    }
+
+    fun addMonth(d: Int): YCalendar {
+        add(MONTH, d)
+        return this
+    }
+
+    fun addYear(d: Int): YCalendar {
+        add(YEAR, d)
+        return this
+    }
+
+    private fun firstDay(): YCalendar {
+        set(DATE, 1)
+        return this
+    }
+
+    fun middleDay(): YCalendar {
+        set(DATE, 15)
+        return this
+    }
+
+    fun lastDay(): YCalendar {
+        add(MONTH, 1)
+        firstDay()
+        add(DATE, -1)
+        return this
+    }
+
+    fun firstMondayOfMonth() {
+        set(DATE, 1)
+        add(DATE, -1)
+        nextMonday()
+    }
+
+    fun monday(): String {
+        add(DATE, -7)
+        nextMonday()
+        return getYYYYMMDD()
+    }
+
+    private fun nextMonday() {
+        nextDayOfWeek(MONDAY)
+    }
+
+    fun dayOfWeek(dayOfWeek: Int) {
+        add(DATE, -7)
+        nextDayOfWeek(dayOfWeek)
+    }
+
+    private fun nextDayOfWeek(dayOfWeek: Int) {
+        while (true) {
+            add(DAY_OF_MONTH, 1)
+            if (get(DAY_OF_WEEK) == dayOfWeek) {
+                break
+            }
+        }
+    }
+
+    fun putAGDInfoExtra(intent: Intent) {
+        intent.putExtra("agd_name", agd_name)
+        intent.putExtra("agd_action1_name", agd_action1_name)
+        intent.putExtra("agd_action_name", agd_action_name)
+        intent.putExtra("agd_action_time", agd_action_time)
+    }
+
+    fun setYMD(c: YCalendar) {
+        setYMD(c.getYear(), c.getMonth(), c.getDate())
+    }
+
+    //
+    // static Methods
+    //
+
+    companion object {
+
+        fun displayYMDHMS(): String {
+            return YCalendar().getYMDHMS()
+        }
+
+        fun displayTime(s: String): String {
+            val c = YCalendar(s)
+            return if (c.getYMDHMS() != s) {
+                s
+            } else {
+                c.toString()
+            }
+        }
+
+        fun displayTime(): String {
+            return YCalendar().toString()
+        }
+
+        fun displayYMDE(s: String): String {
+            val c = YCalendar(s)
+            return if (c.getYYYYMMDD() != (s + "12345678").substring(0, 8)) {
+                s
+            } else {
+                c.getYMDE()
+            }
+        }
+
+        fun displayYMDE(): String {
+            return YCalendar().getYMDE()
+        }
+
+        fun displayYYYY_MM_DD(s: String): String {
+            val c = YCalendar(s)
+            return if (c.getYYYYMMDD() != (s + "12345678").substring(0, 8)) {
+                s
+            } else {
+                c.getYYYY_MM_DD()
+            }
+        }
+
+        fun displayYYYY_MM_DD(): String {
+            return YCalendar().getYYYY_MM_DD()
+        }
+
+        fun displayYYYYMMDD(s: String): String {
+            val c = YCalendar(s)
+            return if (c.getYYYYMMDD() != (s + "12345678").substring(0, 8)) {
+                s
+            } else {
+                c.getYYYYMMDD()
+            }
+        }
+
+        fun displayYYYYMMDD(): String {
+            return YCalendar().getYYYYMMDD()
+        }
+
+        fun isWeekend(cal: YCalendar): Boolean {
+            return cal.get(DAY_OF_WEEK) == SATURDAY || cal.get(DAY_OF_WEEK) == SUNDAY
+        }
+
+        fun isInSameWeek(c1: YCalendar, c2: YCalendar): Boolean {
+            val cal1 = YCalendar(c1)
+            val cal2 = YCalendar(c2)
+            return cal1.monday() == cal2.monday()
+        }
+
+        fun getMonthsBetween(d1: YCalendar, d2: YCalendar): Int {
+            val from = YCalendar(d1)
+            val to = YCalendar(d2)
+            from.setDate(1)
+            from.setHMS(0, 0, 0)
+            to.setDate(1)
+            to.setHMS(0, 0, 0)
+            if (from > to) return -1
+            var rtn = 0
+            while (from < to) {
+                from.addMonth(1)
+                rtn++
+            }
+            return rtn
+        }
+
+        fun getDaysBetween(d1: YCalendar, d2: YCalendar): Int {
+            val from: YCalendar
+            val to: YCalendar
+            if (d1 < d2) {
+                from = YCalendar(d1.getYYYYMMDD())
+                to = YCalendar(d2.getYYYYMMDD())
+            } else {
+                from = YCalendar(d2.getYYYYMMDD())
+                to = YCalendar(d1.getYYYYMMDD())
+            }
+            var rtn = 0
+            while (from < to) {
+                from.addDate(1)
+                rtn++
+            }
+            return rtn
+        }
+
+        fun getWeekCountOfMonth(year: Int, month: Int): Int {
+            val cal = YCalendar()
+            cal.set(year, month, 1)
+            cal.set(DAY_OF_MONTH, cal.getActualMaximum(DAY_OF_MONTH))
+            return cal.get(WEEK_OF_MONTH)
+        }
+
+        fun getMondaysOfMonth(year: Int, month: Int): Array<YCalendar> {
+            val mondays = arrayOf<YCalendar>()
+            val cal = YCalendar(year, month)
+            var monday_index = 0
+            for (d in 1..cal.getActualMaximum(DAY_OF_MONTH)) {
+                cal.set(DAY_OF_MONTH, d)
+                if (cal.get(DAY_OF_WEEK) == MONDAY) {
+                    mondays[monday_index++] = YCalendar(year, month, d)
+                }
+            }
+            return mondays
+        }
+
+        fun createHHMM(ss: String): YCalendar {
+            val c1 = YCalendar("HHmm", ss)
+            val today = YCalendar()
+
+            c1.setYMD(today)
+            c1.setSecond(0)
+
+            return c1
+        }
+    }
+
+
+}
